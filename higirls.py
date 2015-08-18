5e594ce01f0c76e2e683fbd25d1817f55e17918f
angele-smell <angele.zam@gmail.com>
2015-08-17 22:17:02 -0700

    my django girls tut app, first commit

---
+++ higirls.py
@@ -0,0 +1,59 @@
+# higirls.py
+def hi(name)9: '!')
+
+girls = ['Rachel','Monica','Pheobe','Ola','Angele']
+for name in girls:
+    hi(name)
+    print('Next girl')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
